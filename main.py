54da3eafd851107fb8ee7c8e6e008f8e5cadd00a
Alex Marley <amarley99@gmail.com>
2022-02-21 17:45:48 -0500

    normalized signal function

--- main.py
+++ main.py
@@ -2,3 +2,19 @@ import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 
+test_signal = [4, 5, 6, 7, 6, 5]
+
+
+def normalize_signal(signal):
+    sig_maxval = np.max(signal)
+    sig_minval = np.min(signal)
+    sig_norm = np.zeros(len(signal))
+    for i in range(len(signal)):
+        sig_norm[i] = (signal[i] - sig_minval) / (sig_maxval - sig_minval)
+    print(sig_norm)
+
+def remove_extrema(signal):
+
+
+
+normalize_signal(test_signal)
